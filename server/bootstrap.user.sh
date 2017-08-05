60aa49bbc01641b6a6aa2c2a4144d84d8f591154
Vít Kutný <vitkutny@icloud.com>
2017-08-05 19:42:10 +0200

    Vagrant: removed in favor of docker

--- server/bootstrap.user.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-grep -q "cd /vagrant" ~/.profile 2> "/dev/null" || echo "cd /vagrant" >> ~/.profile
-ssh-keyscan -H "github.com" >> ~/.ssh/known_hosts
-ssh-keyscan -H "vitkutny.cz" >> ~/.ssh/known_hosts
-
-cd "/vagrant"
-#dep build vagrant
--- server/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-service php7.1-fpm restart
-service nginx restart
-
